190f5a2da20cd0f4f90c1217ea4a6788bb4244c7
EmmanuelD69 <emmanueldevfr@gmail.com>
2020-05-26 18:38:08 +0200

    création class Drumkit + objet drumkit

---
+++ app.js
@@ -0,0 +1,25 @@
+class Drumkit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.clapAudio = document.querySelector(".clap-sound");
+    this.crashAudio = document.querySelector(".crash-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.openhatAudio = document.querySelector(".openhat-sound");
+    this.percAudio = document.querySelector(".perc-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.tomAudio = document.querySelector(".tom-sound");
+    this.otherAudio = document.querySelector(".other-sound");
+    this.index = 0;
+  }
+  repeat() {
+    let step = this.index % 8;
+    console.log(step);
+  }
+  start() {
+    setInterval(() => {
+      this.repeat();
+    }, 1000);
+  }
+}
+const drumkit = new Drumkit();
