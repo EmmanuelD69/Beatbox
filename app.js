891897808af2ed6aeff4ee8533b870ee40b7db69
EmmanuelD69 <emmanueldevfr@gmail.com>
2020-05-26 19:45:34 +0200

    parametrage du bouton "play"

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 class Drumkit {
   constructor() {
+    this.playBtn = document.querySelector(".play");
     this.pads = document.querySelectorAll(".pad");
     this.kickAudio = document.querySelector(".kick-sound");
     this.clapAudio = document.querySelector(".clap-sound");
@@ -11,11 +12,11 @@ class Drumkit {
     this.tomAudio = document.querySelector(".tom-sound");
     this.otherAudio = document.querySelector(".other-sound");
     this.index = 0;
-    this.bpm = 150;
+    this.bpm = 126;
   }
   repeat() {
     let step = this.index % 8;
-    const activeBars = document.querySelectorAll(`.b${step}`);
+    const activeBars = document.querySelectorAll(`.B${step}`);
     this.index++;
     console.log(activeBars);
   }
@@ -23,8 +24,9 @@ class Drumkit {
     const interval = (60 / this.bpm) * 1000;
     setInterval(() => {
       this.repeat();
-    }, 1000);
+    }, interval);
   }
 }
-const drumkit = new Drumkit();
-drumkit.start();
+const drumKit = new Drumkit();
+
+drumKit.playBtn.addEventListener("click", () => drumKit.start());
